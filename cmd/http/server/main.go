4196cfaa1164e2fb6c6d418bc2cdef4b49fe64d2
Gabriel Silva Malheiro <gsmalheiro@br000c775nxmcwy.ml.com>
2025-04-23 21:23:38 -0300

    feat: add simple project

---
+++ cmd/http/server/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", indexHandler)
+	port := "8080"
+	log.Printf("Listening on port %s", port)
+	log.Printf("Open http://localhost:%s in the browser", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	_, err := fmt.Fprint(w, "Hello, World!")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+}
